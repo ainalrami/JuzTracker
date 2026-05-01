6c31b6009b50cf85e780b0061096dc9ba4e374b1
Talha Berktan Baş <berktan.bas@ug.bilkent.edu.tr>
2026-05-01 19:18:12 +0300

    first commit

---
+++ backend/src/main/java/me/ainalrami/juztracker/backend/JuzTrackerBackendApplication.java
@@ -0,0 +1,11 @@
+package me.ainalrami.juztracker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuzTrackerBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JuzTrackerBackendApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/me/ainalrami/juztracker/backend/JuzTrackerBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package me.ainalrami.juztracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuzTrackerBackendApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
